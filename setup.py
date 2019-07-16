846f6b4930d1dfd853707aa3f7037e312307a9fb
Anatoli <afedynitch@gmail.com>
2019-07-16 05:03:57 +0000

    scikit-build initial setup

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python
+import sys
+from skbuild import setup
 
-from distutils.core import setup
+
+# Require pytest-runner only when running tests
+pytest_runner = (['pytest-runner>=2.0,<3dev']
+                 if any(arg in sys.argv for arg in ('pytest', 'test'))
+                 else [])
+
+setup_requires = pytest_runner
 
 setup(
     name='MCEq',
-    version='0.99',
+    version='1.0.0',
     description='Numerical cascade equation solver',
     author='Anatoli Fedynitch',
     author_email='afedynitch@gmail.com',
-    url='https://github.com/afedynitch/MCEq.git',
+    url='https://github.com/afedynitch/MCEq',
     packages=[
-        'MCEq',
+        'MCEq','nrlmsis00'
     ],
     py_modules=['mceq_config'],
-    requires=['numpy', 'scipy', 'numba'])
+    requires=['numpy', 'scipy', 'numba', 'mkl', 'particletools', 'crflux'])
